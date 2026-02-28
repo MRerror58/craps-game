04c8ff39c0237a96e0df81d37d94e4af1468fa60
juan david <juandavidvasquez28@gmail.com>
2026-02-28 16:50:50 -0500

    add crap vew

--- src/main/java/com/example/crapsgame/HelloApplication.java
+++ src/main/java/com/example/crapsgame/HelloApplication.java
@@ -2,7 +2,11 @@ package com.example.crapsgame;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,9 +14,17 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        // FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+
+
+        VBox root = new VBox();
+        Button helloButton = new Button("HOLA HDP");
+        Label label = new Label("Escriba algo");
+        root.getChildren().add(label);
+        root.getChildren().add(helloButton);
+
+        Scene scene = new Scene(root, 320, 240);
+        stage.setTitle("Ventana");
         stage.setScene(scene);
         stage.show();
     }
